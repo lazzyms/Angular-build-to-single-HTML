b2e628cf4e0c8688bba16a2445fca7d6bb4d5b81
Maulik Sompura <maulik.sompura06@gmail.com>
2018-05-21 17:15:03 +0530

    gulpfile created

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var inlinesource = require('gulp-inline-source');
+var replace = require('gulp-replace');
+var rename = require("gulp-rename");
+
+gulp.task('inlinesource', function () {
+  return gulp.src('./dist/main.html')
+    .pipe(inlinesource())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('../desktop/MCare/Html/'));
+});
+
+gulp.task('replace', function () {
+  gulp.src(['./dist/index.html'])
+    .pipe(replace('<link href', '<link inline href'))
+    .pipe(replace('<script type', '<script inline type'))
+    .pipe(rename('main.html'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['replace', 'inlinesource']);
